09a6899ef68ade219e4b6c99b94931fb0b1aae85
drewkiimon <andrew.r.pagan@gmail.com>
2020-10-16 15:39:59 -0700

    Able to add and remove Todos in the UI

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,11 +3,14 @@ import { useHistory } from 'react-router-dom';
 import UserContext from '../../context/UserContext';
 import Axios from 'axios';
 
+import ErrorNotice from '../misc/ErrorNotice';
 import Todo from '../misc/Todo';
 
 export default function Home() {
 	const { userData } = useContext(UserContext);
 	const [todos, setTodos] = useState([]);
+	const [todo, setTodo] = useState('');
+	const [error, setError] = useState();
 	const history = useHistory();
 
 	useEffect(() => {
@@ -34,8 +37,47 @@ export default function Home() {
 		getAllTodos();
 	}, []);
 
+	const submit = async (e) => {
+		try {
+			e.preventDefault();
+			let token = localStorage.getItem('auth-token');
+
+			var returnedTodo = await Axios.post(
+				'http://localhost:5000/todos/',
+				{ title: todo },
+				{
+					headers: {
+						'x-auth-token': token,
+					},
+				}
+			);
+
+			setTodo('');
+			setTodos([...todos, returnedTodo.data]);
+		} catch (err) {
+			err.response.data.msg && setError(err.response.data.msg);
+		}
+	};
+
 	return (
 		<div className='page'>
+			{error && (
+				<ErrorNotice
+					message={error}
+					clearError={() => setError(undefined)}
+				/>
+			)}
+
+			<form className='form' onSubmit={submit}>
+				<label htmlFor='addTodo'></label>
+				<input
+					type='text'
+					id='addTodo'
+					value={todo}
+					onChange={(e) => setTodo(e.target.value)}></input>
+				<input type='submit' value='Add Todo' />
+			</form>
+
 			<ul>
 				{todos.length > 0 &&
 					todos.map((item, i) => {

7e2cb75e8af83278e5512c0e46fd815beef4dbe3
drewkiimon <andrew.r.pagan@gmail.com>
2020-10-18 14:19:18 -0700

    Updates

---
+++ precache-manifest.03b0669a7584ad7a6ff7941dc5330af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25249c66f867de06165b42a62581f26",
+    "url": "/mern-auth-frontend/index.html"
+  },
+  {
+    "revision": "2dff03b159a21d636276",
+    "url": "/mern-auth-frontend/static/css/main.9cc3c86a.chunk.css"
+  },
+  {
+    "revision": "bdd53b3ca94f386f65ac",
+    "url": "/mern-auth-frontend/static/js/2.a87d4296.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/mern-auth-frontend/static/js/2.a87d4296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dff03b159a21d636276",
+    "url": "/mern-auth-frontend/static/js/main.3d19d617.chunk.js"
+  },
+  {
+    "revision": "9f24f01d4ad467f104a6",
+    "url": "/mern-auth-frontend/static/js/runtime-main.3211440b.js"
+  }
+]);
